bf18debfe9fc01b21809cbc1bae6881ce48b2e19
verybag <82975666+verybag@users.noreply.github.com>
2022-01-05 00:27:49 -0500

    Added files

---
+++ quotes.js
@@ -0,0 +1,11 @@
+export const Quotes = [
+  'fucking losers',
+  'no sound',
+  'controller player',
+  '$200 headphones',
+  'bot',
+  'degen',
+  'sheeeeeeeeeesh',
+  'retards',
+  'where are my shots',
+];
